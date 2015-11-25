167e57017dd1c75cb1bdd165b7bb8c954241d973
Jisan Zaman <jzaman@wesleyan.edu>
2015-11-24 23:59:59 -0500

    added governors json, putting in statements

--- governorrefugees/views/map/map.js
+++ governorrefugees/views/map/map.js
@@ -6,18 +6,46 @@ angular.module('map', ['ngRoute'])
     controller: 'MapController'
    })
 }])
-.controller('MapController',['$scope',function($scope){
+.controller('MapController',['$scope', '$http', function($scope, $http){
   console.log('its getting here');
 
-  $('#map').usmap({
-    // The click action
-    click: function(event, data) {
-      $('#clicked-state')
-        .text('You clicked: '+data.name)
-        
-    },
-    stateStyles: {fill: 'white'},
-    stateHoverStyles: {fill : 'red'}
-  });
+  
+
+
+  $http.get("/data/governors.json", {cache: true})
+    .error(function(err){
+      console.error(err);
+    })
+    .success(function(data){
+      console.log(data);
+      $scope.governors = data;
+      var stateObj = {}
+      for(var key in data){
+        var color;
+        if(data[key].Party === 'Democratic'){
+          color = 'blue'
+        }else if(data[key].Party === 'Republican'){
+          color = 'red';
+        }else{
+          color = 'yellow';
+        }
+        stateObj[key] = { fill : color }
+      }
+      $('#map').usmap({
+        // The click action
+        click: function(event, data) {
+          if($scope.governors){
+            // console.log($scope.governors[data.name]);
+            $scope.selectedState = $scope.governors[data.name];
+            $scope.$apply();
+          }
+            
+        },
+        stateStyles: {fill: 'white'},
+        stateHoverStyles: {fill : 'green'},
+        stateSpecificStyles : stateObj
+          
+      });
+    })
 
 }])
